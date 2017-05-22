432e1c8883168dc513e0a1033987f18fdb769582
Niu <alexniu@amazon.com>
2017-05-22 17:24:58 -0400

    Changed the ordering of configuration loadin, added additional debug logs to LogToJSONDataConverter and included extra depenedencies in the pom file

--- src/com/amazon/kinesis/streaming/agent/processing/processors/LogToJSONDataConverter.java
+++ src/com/amazon/kinesis/streaming/agent/processing/processors/LogToJSONDataConverter.java
@@ -1,14 +1,14 @@
 /*
  * Copyright 2014-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Amazon Software License (the "License").
- * You may not use this file except in compliance with the License. 
+ * You may not use this file except in compliance with the License.
  * A copy of the License is located at
- * 
+ *
  *  http://aws.amazon.com/asl/
- *  
- * or in the "license" file accompanying this file. 
- * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ *
+ * or in the "license" file accompanying this file.
+ * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
 package com.amazon.kinesis.streaming.agent.processing.processors;
@@ -26,10 +26,12 @@ import com.amazon.kinesis.streaming.agent.processing.interfaces.IDataConverter;
 import com.amazon.kinesis.streaming.agent.processing.interfaces.IJSONPrinter;
 import com.amazon.kinesis.streaming.agent.processing.interfaces.ILogParser;
 import com.amazon.kinesis.streaming.agent.processing.utils.ProcessingUtilsFactory;
+import org.slf4j.Logger;
+import com.amazon.kinesis.streaming.agent.Logging;
 
 /**
  * Parse the log entries from log file, and convert the log entries into JSON.
- * 
+ *
  * Configuration of this converter looks like:
  * {
  *     "optionName": "LOGTOJSON",
@@ -37,17 +39,19 @@ import com.amazon.kinesis.streaming.agent.processing.utils.ProcessingUtilsFactor
  *     "matchPattern": "OPTIONAL_REGEX",
  *     "customFieldNames": [ "column1", "column2", ... ]
  * }
- * 
+ *
  * @author chaocheq
  *
  */
 public class LogToJSONDataConverter implements IDataConverter {
-    
+
+    protected final Logger logger;
     private List<String> fields;
     private ILogParser logParser;
     private IJSONPrinter jsonProducer;
-    
+
     public LogToJSONDataConverter(Configuration config) {
+        this.logger = Logging.getLogger(getClass());
         jsonProducer = ProcessingUtilsFactory.getPrinter(config);
         logParser = ProcessingUtilsFactory.getLogParser(config);
         if (config.containsKey(ProcessingUtilsFactory.CUSTOM_FIELDS_KEY)) {
@@ -58,22 +62,24 @@ public class LogToJSONDataConverter implements IDataConverter {
     @Override
     public ByteBuffer convert(ByteBuffer data) throws DataConversionException {
         String dataStr = ByteBuffers.toString(data, StandardCharsets.UTF_8);
-        
+
         // Preserve the NEW_LINE at the end of the JSON record
         if (dataStr.endsWith(NEW_LINE)) {
             dataStr = dataStr.substring(0, (dataStr.length() - NEW_LINE.length()));
         }
-        
+
         Map<String, Object> recordMap;
-        
+
         try {
+            logger.debug("Parsing Line: " + dataStr);
             recordMap = logParser.parseLogRecord(dataStr, fields);
         } catch (LogParsingException e) {
             // ignore the record if a LogParsingException is thrown
             // the record is filtered out in this case
+            logger.debug("Exception while parsing " + dataStr + ": " + e.toString());
             return null;
         }
-        
+
         String dataJson = jsonProducer.writeAsString(recordMap) + NEW_LINE;
         return ByteBuffer.wrap(dataJson.getBytes(StandardCharsets.UTF_8));
     }
